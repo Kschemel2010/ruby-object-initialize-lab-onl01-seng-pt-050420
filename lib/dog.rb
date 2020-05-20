e28a3cee47a28f630b99087949b27d34aa9433d4
kschemel2010 <kschemel2010@gmail.com>
2020-05-20 00:35:25 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,27 @@
 class Dog
-    attr_accessor :name, :breed
 
-    def initialize(name, breed)
+    def initialize(name, breed = "Mutt")
       @name, @breed = name, breed
     end
+
+    def name=(name)
+        @name=name
+    end
+
+    def name
+        @name
+    end
+
+    def breed=(breed)
+        @breed=breed
+    end
+
+    def breed
+        @breed
+    end
+
 end
 
-hello = Dog.new
 
 
 
@@ -14,22 +29,4 @@ hello = Dog.new
 
 
 
-    # def name=(dog_name)
 
-#     this_dogs_name = dog_name
-#   end
- 
-#   def name
-#     this_dogs_name
-#   end
-
-#   def breed=(dog_breed)
-#     this_dogs_breed = dog_breed
-#   end
- 
-#   def name
-#     this_dogs_breed
-#   end
-# end
-
-# fido = Dog.new("Fido")
